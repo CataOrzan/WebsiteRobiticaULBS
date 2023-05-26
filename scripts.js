156221d5e1b89af38b1bd079a3113b2c69e61e0f
CataOrzan <orzancatalinm@gmail.com>
2023-05-26 12:59:00 +0300

    Updated_smalll_screen

--- scripts.js
+++ scripts.js
@@ -14,3 +14,8 @@ function call() {
   document.getElementById("art").innerHTML = parseFloat(avg).toFixed(2);
   document.getElementById("pnd").innerHTML = parseFloat(pond).toFixed(2);
 }
+
+function toggleULBS() {
+  var ulbs = document.querySelector('.ulbs');
+  ulbs.style.display = document.getElementById('active').checked ? 'none' : 'block';
+}
